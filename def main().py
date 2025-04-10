79e880f7352181e783a10346ab217ff79244820d
“kevin3599” <189818185@qq.com>
2025-04-10 10:24:27 -0700

    提交

--- def main().py
+++ def main().py
@@ -1,5 +1,10 @@
+
 def main():
-    num = int(input())
-    for i in range ((num+2)):
-        print(num,end=" ")
+   num = int(input())
+   for i in range ((num+2)):
+       print(num,end=" ")
 main()
+def compute_val():
+    input1 = int(input())
+    print(f" {input1}"*(input1+2))
+compute_val()
