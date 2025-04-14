bc96e463cac08c3fabb352b31003d6ff5150baa4
“kevin3599” <189818185@qq.com>
2025-04-14 08:09:06 -0700

    提交

---
+++ shortcut.py
@@ -0,0 +1,8 @@
+#short cut
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+num1 += num2     # num1 = num1 + num2
+num1 /= 2        # num1 = num1 / 2
+
+print("Average is:", num1)
