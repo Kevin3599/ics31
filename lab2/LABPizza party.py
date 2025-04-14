def877a0f32f68eaf06ed7cac52290fcd797e3e8
“kevin3599” <189818185@qq.com>
2025-04-14 09:08:12 -0700

    提交

--- lab2/LABPizza party.py
+++ lab2/LABPizza party.py
@@ -3,6 +3,6 @@ from math import *
 people = int(input())
 num_pizzas = ceil(people*2 / 12)
 cost = num_pizzas * 14.95
-print(f"people: {people}")
-print(f'pizza(s): {num_pizzas}')
+print(f"People: {people}")
+print(f'Pizza(s): {num_pizzas}')
 print(f'Cost for {num_pizzas} pizza(s): ${cost:.2f}')
--- lab2/calories burned.py
+++ lab2/calories burned.py
@@ -1,11 +1,11 @@
 # LAB: Expression for calories burned during workout
-def calories(age, weight, heart_rate, time):
-    return ((age * 0.2757) + (weight * 0.03295) + (heart_rate * 1.0781) - 75.4991) * time / 8.368
+def calories(age, wght, h_rate, time):
+    return ((age * 0.2757) + (wght * 0.03295) + (h_rate * 1.0781) - 75.4991) * time / 8.368
 
 age = float(input())
-weight = float(input())
-heart_rate = float(input())
+wght = float(input())
+h_rate = float(input())
 time = float(input())
 
-result = calories(age, weight, heart_rate, time)
+result = calories(age, wght, h_rate, time)
 print(f'Calories: {result:.2f} calories')
