1b15d4845fcef6e77c22c13b7f0a553240216877
“kevin3599” <189818185@qq.com>
2025-04-14 08:55:55 -0700

    提交

--- lab2/food recipt.py
+++ lab2/food recipt.py
@@ -2,34 +2,33 @@ items = []
 prices = []
 quantities = []
 
+print("Enter food item name:")
+items.append(input())
+print("Enter item price:")
+prices.append(float(input()))
+print("Enter item quantity:")
+quantities.append(int(input()))
+
+print("\nRECEIPT")
+cost = prices[0] * quantities[0]
+print(f"{quantities[0]} {items[0]} @ ${prices[0]:.2f} = ${cost:.2f}")
+print(f"Total cost: ${cost:.2f}\n")
+
+print("Enter second food item name:")
+items.append(input())
+print("Enter item price:")
+prices.append(float(input()))
+print("Enter item quantity:")
+quantities.append(int(input()))
+
+print("\nRECEIPT")
 for i in range(2):
-    if i == 1:
-        print("\nEnter second food item name:")
-    else:
-        print("Enter food item name:")
-    items.append(input())
-    
-    print("Enter item price:")
-    prices.append(float(input()))
-    
-    print("Enter item quantity:")
-    quantities.append(int(input()))
+    cost = prices[i] * quantities[i]
+    print(f"{quantities[i]} {items[i]} @ ${prices[i]:.2f} = ${cost:.2f}")
 
-def receipt(items, prices, quantities):
-    print("\nRECEIPT")
-    subtotal = 0
-    
-    for i in range(2):
-        cost = prices[i] * quantities[i]
-        subtotal += cost
-        print(f"{quantities[i]} {items[i]} @ ${prices[i]:.2f} = ${cost:.2f}")
+total_cost = sum(p * q for p, q in zip(prices, quantities))
+gratuity = total_cost * 0.15
 
-    tip = subtotal * 0.15
-    total = subtotal + tip
-    
-
-    print(f"Subtotal: ${subtotal:.2f}")
-    print(f"15% tip: ${tip:.2f}")
-    print(f"Total with tip: ${total:.2f}")
-
-receipt(items, prices, quantities)
+print(f"Total cost: ${total_cost:.2f}")
+print(f"\n15% gratuity: ${gratuity:.2f}")
+print(f"Total with tip: ${total_cost + gratuity:.2f}")
