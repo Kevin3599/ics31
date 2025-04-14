5e550ddbe0a1feee59e7bdc12f75980d555b9201
“kevin3599” <189818185@qq.com>
2025-04-14 08:41:07 -0700

    修改

--- lab2/# (1) Get inputs.py
+++ lab2/# (1) Get inputs.py
@@ -1,14 +1,7 @@
-# (1) Get inputs
-int_val = int(input('Enter integer (32 - 126):\n'))
-float_val = float(input('Enter float:\n'))
-char_val = input('Enter character:\n')
-string_val = input('Enter string:\n')
-
-# Output in original order
-print(f"{int_val} {float_val} {char_val} {string_val}")
-
-# (2) Output in reverse order
-print(f"{string_val} {char_val} {float_val} {int_val}")
-
-# (3) Convert integer to character
-print(f"{int_val} converted to a character is {chr(int_val)}")
+user_int = int(input('Enter integer (32 - 126):\n'))
+float_val = float(input('Enter float: \n'))
+char = input('Enter character:\n')
+str = str(input('Enter string:\n'))
+print(f"{user_int} {float_val} {char} {str}")
+print(f"{str} {char} {float_val} {user_int}")
+print(f"{user_int} converted to a character is {chr(user_int)}")
