9b7493486780077e5fb5892e3ffab0fcff89fe90
Kevin3599 <189818185@qq.com>
2025-04-15 01:47:42 -0700

    草稿本

---
+++ #zybook.py
@@ -0,0 +1,6 @@
+#zybook
+# Modify the following line of code
+import math
+sphere_radius = float(input())
+sphere_vol = 4/3 * math.pi * sphere_radius * sphere_radius * sphere_radius
+print(f'{sphere_vol:.2f}')
