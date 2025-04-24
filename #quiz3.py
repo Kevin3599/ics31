909575d3d5fb21207d5b6c81c3f76b383c891e0a
“kevin3599” <189818185@qq.com>
2025-04-24 10:27:28 -0700

    quiz3

---
+++ #quiz3.py
@@ -0,0 +1,10 @@
+#quiz3
+usr = input("are you doing trick-or-treating? (yes or no) ")    
+if usr == "yes":
+    print("Happy Halloween!")
+elif usr == "no":
+    print("That's okay, you can still enjoy the season!")
+elif usr == 'yes or no':
+   print("8")
+else:
+    print("Invalid input. Please answer with 'yes' or 'no'.")
