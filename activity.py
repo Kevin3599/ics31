901cc5325ae830d03ef3f1b74f84bba51e408990
“kevin3599” <189818185@qq.com>
2025-04-27 20:47:44 -0700

    提交

---
+++ activity.py
@@ -0,0 +1,10 @@
+x = int(input("whats your favorite positive integer? "))
+
+
+if x == 13:
+    print('x' /n 'thats my favorite number too!'/n'what a coincidence!')
+
+elif x > 30 or x < 10:
+    print('x' /n 'thats a big number!'/n'what a coincidence!')
+else:
+    print('x' /n 'thats a small number!'/n'what a coincidence!')
